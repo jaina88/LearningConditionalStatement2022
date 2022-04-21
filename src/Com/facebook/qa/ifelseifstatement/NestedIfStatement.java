4a186b2f618577cbe9bcc0d13e2863c8c8aca2cb
jaina88 <jainapatel88@gmail.com>
2022-04-20 21:41:15 -0400

    changes files

---
+++ src/Com/facebook/qa/ifelseifstatement/NestedIfStatement.java
@@ -0,0 +1,30 @@
+package Com.facebook.qa.ifelseifstatement;
+
+public class NestedIfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 20;
+		
+		int weight_lbs =80;
+		
+		if(age>=18) {
+			 
+			if (weight_lbs>50) {
+			 
+				System.out.println("you are eligible to donate blood");
+			}
+			
+			
+	
+	
+	
+		}
+		
+		
+		
+		
+	}
+
+}
